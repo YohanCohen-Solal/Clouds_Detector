22fdaef57f5b1375c6ff81f9afc67e6cde56116e
JEOS <81352535+EOS5@users.noreply.github.com>
2022-11-23 20:41:02 +0100

    build datasets

--- train.py
+++ train.py
@@ -1,11 +1,22 @@
 from unet import UNet
-import pathlib
+from torch.utils import data
+from torchvision import datasets, transforms
 
+import numpy as np
+import pathlib
 import os
  
 path = pathlib.Path('Data')
-
 data_dir = os.listdir(path)
 
-def create_tataset():
-    
+#creating datasets
+dataset = datasets.ImageFolder(path, transform=transforms.ToTensor())
+lengths = [int(np.ceil(0.8*len(dataset))),
+           int(np.floor(0.2*len(dataset)))]
+train_set, test_set = data.random_split(dataset, lengths)
+
+#create dataloaders
+train_dataloader = data.DataLoader(train_set, batch_size=20)
+test_dataloader = data.DataLoader(test_set, batch_size=20)
+
+#def train():
