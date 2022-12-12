bf9d6c3708d925fe46846d4b7e7fce4f0eadf51a
JEOS <81352535+EOS5@users.noreply.github.com>
2022-12-12 20:19:00 +0100

    fixed slow predict

--- IA/unet/Data_Segmented.py
+++ IA/unet/Data_Segmented.py
@@ -12,12 +12,21 @@ model = UNet(n_channels=3, n_classes=1)
 
 model.load_state_dict(torch.load('my_checkpoint.pth.h5')['state_dict'])
 model.eval()
-img_path = "IA/Data/stratus/stratocumulus.jpeg"
+img_path = "IA/Data/cumulus/Small_puffy_cumulus_clouds.jpg"
 pil_image = Image.open(img_path)
+width, height = pil_image.size
+
+# Définissez les nouvelles dimensions de l'image
+new_width = 240
+new_height = 240
+
+# Redimensionnez l'image
+pil_image = pil_image.resize((new_width, new_height), Image.ANTIALIAS)
+
 
 to_tensor = transforms.ToTensor()
 tensor_image = to_tensor(pil_image)
 tensor_image = tensor_image.unsqueeze(0)
 preds = torch.sigmoid(model(tensor_image))
 
-torchvision.utils.save_image(preds, "pred_.png")
+torchvision.utils.save_image(preds, "pred_1.png")
