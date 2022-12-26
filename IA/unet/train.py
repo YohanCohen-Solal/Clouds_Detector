2a27802908c02f5dee78a5cd4981758db27717a8
JEOS <81352535+EOS5@users.noreply.github.com>
2022-12-26 19:48:47 +0100

    adding some hyperparameter change

--- IA/unet/train.py
+++ IA/unet/train.py
@@ -18,7 +18,7 @@ from utils import (
 LEARNING_RATE = 1e-4
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 BATCH_SIZE = 3
-NUM_EPOCHS = 3
+NUM_EPOCHS = 30
 NUM_WORKERS = 2
 IMAGE_HEIGHT = 240
 IMAGE_WIDTH = 240
